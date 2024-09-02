aaeaa60670a3b4522797ff378547562dcb7464fa
Ankur Patel <32640688+ankur51206@users.noreply.github.com>
2024-09-02 14:47:24 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,3 @@
-# provider.tf
 provider "azurerm" {
   features {}
 
@@ -6,4 +5,13 @@ provider "azurerm" {
   client_secret   = var.client_secret
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "realimmerse_group"    # Replace with your resource group
+    storage_account_name = "realimmersegroupdiag"     # Your storage account name
+    container_name       = "tfstate"          # The container name created in Azure Storage
+    key                  = "terraform.tfstate"         # The name of the state file
+  }
 }
